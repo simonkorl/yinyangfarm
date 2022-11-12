efc475336aaee03c743831688c4c8d783859c08a
simonkorl <mc21@mails.tsinghua.edu.cn>
2022-11-12 15:55:56 +0800

    feat: create simplest game loop
    add prototype building, terminal interaction function

--- prototype1.py
+++ prototype1.py
@@ -1,4 +1,6 @@
+import logging
 import random
+logging.basicConfig(level=logging.INFO)
 class YinYang:
     '''
     basic class for yin yang state
@@ -43,6 +45,14 @@ class Operator(Character):
         self.skills = {}
         Character.__init__(self.base_atk, self.base_dfs, self.base_hp)
 
+def has_enough_resources(resources: dict, recipe: dict) -> bool:
+    for key in recipe:
+        if key not in resources:
+            return False
+        elif resources[key] < recipe[key]:
+            return False
+    return True
+
 class Farm:
     '''
     '''
@@ -60,47 +70,82 @@ class Farm:
         }
         self.blue_script = {
             0: {
-                0: 1,
-                1: 3,
-                2: 5,
-                3: 8,
-            }
+                0: {0: 1}, # level: resources
+                1: {0: 3},
+                2: {0: 5},
+                3: {0: 8}, # max
+            } # facility
         }
         self.items = {"0": None} # a collection of all possible items
 
-    def build(self):
-        print("try to build")
+    def build(self, key):
+        '''
+        a base building function
+        can build and upgrade facilities
+        '''
+        logging.info("try to build")
+        if key in self.production:
+            level = self.production[key]
+            if level >= len(self.blue_script[key]) - 1:
+                logging.info(f"maximum level: {key}({level})")
+                return
+            if has_enough_resources(self.resources, self.blue_script[key][self.production[key]]):
+                logging.info(f"upgrade facility: {key} ({self.production[key]} -> {self.production[key] + 1})")
+                self.production[key] = self.production[key] + 1
+            else:
+                logging.warning("don't have enough resources")
+        elif key in self.blue_script:
+            # try to build the facility
+            if has_enough_resources(self.resources, self.blue_script[key][0]):
+                logging.info("build facility: %s (nil -> 0)" % (key))
+                self.production[key] = 0
+        else:
+            logging.warning("don't have the facility or the blue script: {key}")
         # for key in self.production.keys():
         #     if has_enough_resource():
         #         consume()
         #         upgrade()
 
     def heal(self):
-        print("try to heal operators")
-        pass
+        logging.info("try to heal operators")
     
     def produce(self):
-        print("produce resources")
+        logging.info("produce resources")
+        old_res = self.resources.copy()
         for key in self.production.keys():
             if self.resources.get(key) is not None:
-                self.production_table[key][self.production[key]](self.resources[key])
+                self.resources[key] = self.production_table[key][self.production[key]](self.resources[key])
+        logging.info(f"{old_res} => {self.resources}")
 
-def time_pass(farm, yinyang):
-    print("Current time is: ", "Day" if yinyang.is_yang() else "Night")
+def change_phase(farm, yinyang):
     if yinyang.is_yang():
         # go to the night
         farm.heal()
         farm.produce()
     elif yinyang.is_yin():
         # go to the day
-        farm.build()
+        farm.build(0)
     yinyang.swap()
-    print("time change to %s from %s" % ("Day" if yinyang.is_yang() else "Night", "Night" if yinyang.is_yang() else "Day"))
+    print("time change from %s to %s" % ("Day" if yinyang.is_yang() else "Night", "Night" if yinyang.is_yang() else "Day"))
+    return yinyang.is_yang()
 
 if __name__ == "__main__":
     yinyang = YinYang()
     yinyang.set(1)
     farm = Farm()
-
-    for i in range(3):
-        time_pass(farm, yinyang)
+    days = 1
+    while True:
+        # begin phase
+        yinyang_symbol = "+" if yinyang.is_yang() else "-"
+        print("=== Day (%d)%s Current time is: %s%s ===" % (days, yinyang_symbol, "Light" if yinyang.is_yang() else "Night", yinyang_symbol))
+        command = input("input command (1: change phase, 2: check resources): ")
+        match command:
+            case "1":
+                change_phase(farm, yinyang)
+                if yinyang.is_yang():
+                    days += 1
+            case "2":
+                print(farm.resources)
+            case _:
+                print("invalid command")
+        
